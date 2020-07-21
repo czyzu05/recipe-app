c7a8071452d1f28b6bb2722a490d66df00268b11
czyzu05 <damian.czyz1998@gmail.com>
2020-07-21 16:44:01 +0200

    fetch data from API and display example recipes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Recipe from './Recipe'
 import './App.css'
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'cca26228'
+  const APP_KEY = 'fee8d93a727f3878febeaefe645b2034'
+
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+
+    setRecipes(data.hits)
+  }
+
   return (
     <div className="App">
-      Recipe app
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}  
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   )
 }
